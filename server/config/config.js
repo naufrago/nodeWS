6a48a7559d9884497eb2c35b37b6eea7180d14b0
Yeison Aguirre Osorio <naufragodev@gmail.com>
2020-11-11 17:21:27 -0500

    actualizacion ruta en env de heroku del mongo

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://naufragodev:naufragodev@naufragodevbd.zg9uv.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.urlDB = urlDB;
